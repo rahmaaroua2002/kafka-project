c47d1f0db3032b6e7e80898ddd4c9d2fd38ef8b0
rahmaaroua2002 <rahma.aroua2002@gmail.com>
2024-05-05 08:28:48 +0100

    starting our project

---
+++ demo-spring-cloud-kafka/src/main/java/org/example/demospringcloudkafka/DemoSpringCloudKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.example.demospringcloudkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringCloudKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringCloudKafkaApplication.class, args);
+	}
+
+}
---
+++ demo-spring-cloud-kafka/src/test/java/org/example/demospringcloudkafka/DemoSpringCloudKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demospringcloudkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringCloudKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
